b1912502014fa769fe87a5503779d85d53dd365e
lucifero55 <claudiotazza@gmail.com>
2017-03-12 20:11:17 +0100

    Modifiche

--- main.c
+++ main.c
@@ -5,27 +5,23 @@
 #define NELEM 50
 
 int main(){
-  char c, pwd[NELEM];
+  char c, choose, pwd[NELEM];
   int i = 0, lPass;
 
-  puts("PASSWORD");
+  printf("PASSWORD: ");
   fgets(pwd, NELEM, stdin);
 
   pwd[NELEM - 1] = '\0';
   lPass = strlen(pwd);
 
-  for(; i < lPass; ++i){
-    printf("%c", pwd[i]);
-  }
-  /*while ((c = getchar()) != EOF){
-    if(c >= 65 && c <= 90)
-      c = (c + (pwd[i++] - 'A') - 'A') % 26 + 'A';
+  while ((c = getchar()) != EOF){
+    if(c >= 32 && c <= 126){
+      c = ( (c - ' ') + (pwd[i++] - ' ') ) % 94 + ' ';
+    }
 
-    if(i > lPass) i = 0;
-
+    if(i == lPass) i = 0;
     putchar(c);
-  }*/
-
+  }
 
   return 0;
 }
